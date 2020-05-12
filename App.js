20f46ee2af37bdc60dbc54ffe212830082040a0d
ReygenM <egenskill777>
2020-05-12 17:49:41 +0800

    fix fix

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Global from './component/apiGlobal';
+import ApiGlobal from './component/apiGlobal';
+import ApiIndonesia from './component/apiIndonesia';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-     <Global />
-     
+    <View style={{flex:1}}>
+      <Text style={{alignSelf:'center', fontWeight:'bold' , fontSize:36}}>Data Covid-19</Text>
+      <View style={{flex:2, borderTopWidth:1}}>
+        <ApiGlobal/>
+      </View>
+      <View style={{flex:6.5}}>
+        <ApiIndonesia/>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
